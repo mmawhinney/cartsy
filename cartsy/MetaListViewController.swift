fb1f10a4eb2ff3fba5dccf13bd4d7fd7ab4d3d5b
mathesonm <matheson.mawhinney@gmail.com>
2014-12-13 20:24:15 -0800

    got tableview to display data

--- cartsy/MetaListViewController.swift
+++ cartsy/MetaListViewController.swift
@@ -9,17 +9,38 @@
 import Foundation
 import UIKit
 
-class MetaListViewController: UIViewController {
+class MetaListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var metaListTable: UITableView!
+    var tableData = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        metaListTable.delegate = self
+        metaListTable.dataSource = self
+        tableData.append("Grocery List")
+        tableData.append("Other List")
+        metaListTable.reloadData()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection: Int) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: .Default, reuseIdentifier: "myCell")
+        cell.accessoryType = .DisclosureIndicator
+        var rowData = tableData[indexPath.row]
+        cell.textLabel?.text = rowData
+        return cell
+    }
+
     
     
 }
