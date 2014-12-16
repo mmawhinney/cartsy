9342a19b085205e25e3298b3c7740ee10209bdb0
alexpopov <hello@alexpopov.ca>
2014-12-15 19:12:11 -0800

    Added proper UITableView protocols to GroceryList

--- cartsy/GroceryListViewController.swift
+++ cartsy/GroceryListViewController.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 
-class GroceryListViewController: UIViewController {
+class GroceryListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var groceryListTable: UITableView!
+    var tableData = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        groceryListTable.delegate = self
+        groceryListTable.dataSource = self
+        tableData.append("Green eggs")
+        tableData.append("Ham")
+        groceryListTable.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +29,18 @@ class GroceryListViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: .Default, reuseIdentifier:  "myCell")
+        cell.accessoryType = .DisclosureIndicator
+        var rowData = tableData[indexPath.row]
+        cell.textLabel?.text = rowData
+        return cell
+    }
+    
     /*
     // MARK: - Navigation
 
