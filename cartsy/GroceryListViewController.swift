81f716aed2d3ae2c5db15b2f1859a9f4baaf9b79
alexpopov <hello@alexpopov.ca>
2014-12-17 00:53:24 -0800

    commented code, removed old lines, added // MARK s

--- cartsy/GroceryListViewController.swift
+++ cartsy/GroceryListViewController.swift
@@ -11,65 +11,18 @@ import CoreData
 
 class GroceryListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+	// MARK: IBOutlets
+	
     @IBOutlet weak var groceryListTable: UITableView!
-    //var tableData = [String]()
     var tableData = [NSManagedObject]()
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-        groceryListTable.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell") // what does this do?
-        
-        groceryListTable.delegate = self
-        groceryListTable.dataSource = self
-//        tableData.append("Green eggs")
-//        tableData.append("Ham")
-        // groceryListTable.reloadData() // needed?
-    }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tableData.count
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = UITableViewCell(style: .Default, reuseIdentifier:  "Cell")
-        // cell.accessoryType = .DisclosureIndicator
-//        var rowData = tableData[indexPath.row]
-//        cell.textLabel?.text = rowData
-        let item = tableData[indexPath.row]
-        cell.textLabel!.text = item.valueForKey("name") as String?
-        return cell
-    }
-    
-    func saveName(name: String) -> Void {
-        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
-        let managedContext = appDelegate.managedObjectContext!
-        
-        let entity = NSEntityDescription.entityForName("Item", inManagedObjectContext: managedContext)
-        let item = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
-        
-        item.setValue(name, forKey: "name")
-        
-        var error : NSError?
-        
-        if !managedContext.save(&error) {
-            println("Could not save! \(error)")
-        }
-        
-        tableData.append(item)
-    }
-    
+    // MARK: IBActions
+	
     @IBAction func addButtonPressed(sender: AnyObject) {
         var alert = UIAlertController(title: "Save", message: "Savey!", preferredStyle: .Alert)
         
         let saveAction = UIAlertAction(title: "Save", style: .Default) { (action: UIAlertAction!) -> Void in
             let textField = alert.textFields![0] as UITextField
-//            self.tableData.append(textField.text)
             self.saveName(textField.text)
             self.groceryListTable.reloadData()
         }
@@ -87,6 +40,8 @@ class GroceryListViewController: UIViewController, UITableViewDataSource, UITabl
         presentViewController(alert, animated: true, completion: nil)
     }
     
+    // MARK: Boilerplate Code
+    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
@@ -102,6 +57,55 @@ class GroceryListViewController: UIViewController, UITableViewDataSource, UITabl
         } else {
             println("Could not fetch: \(error)")
         }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        groceryListTable.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell") // what does this do?
+        
+        groceryListTable.delegate = self
+        groceryListTable.dataSource = self
+        // groceryListTable.reloadData() // needed?
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+	// MARK: TableView Delegate Functions
+	
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: .Default, reuseIdentifier:  "Cell")
+        let item = tableData[indexPath.row]
+        cell.textLabel!.text = item.valueForKey("name") as String?
+        return cell
+    }
+	
+	// MARK: Homerolled Functions
+    
+    func saveName(name: String) -> Void {
+        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        let managedContext = appDelegate.managedObjectContext!
+        
+        let entity = NSEntityDescription.entityForName("Item", inManagedObjectContext: managedContext)
+        let item = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+        
+        item.setValue(name, forKey: "name")
+        
+        var error : NSError?
+        
+        if !managedContext.save(&error) {
+            println("Could not save! \(error)")
+        }
+        
+        tableData.append(item)
     }
     
     /*
--- cartsy/MetaListViewController.swift
+++ cartsy/MetaListViewController.swift
@@ -13,23 +13,28 @@ import CoreData
 
 class MetaListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    // MARK: IBOutlets
     @IBOutlet weak var metaListTable: UITableView!
     var tableData = [String]()
+    
+    // MARK: Boiletplate Overrides
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         metaListTable.delegate = self
         metaListTable.dataSource = self
-        tableData.append("Grocery List")
-        tableData.append("Other List")
+        tableData.append("Grocery List")            // TODO: make a tableCreation method
+        tableData.append("Other List")              // TODO: fill table from Core Data
         metaListTable.reloadData()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    // MARK: Table View Delegate Functions
     
     func tableView(tableView: UITableView, numberOfRowsInSection: Int) -> Int {
         return tableData.count
@@ -46,9 +51,11 @@ class MetaListViewController: UIViewController, UITableViewDataSource, UITableVi
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         NSLog("Did select row at index path \(indexPath)")
         tableView.deselectRowAtIndexPath(indexPath, animated: false)
-        let groceryList = self.storyboard?.instantiateViewControllerWithIdentifier("MainList")! as GroceryListViewController // perhaps try cleaning this line.
+        let groceryList = self.storyboard!.instantiateViewControllerWithIdentifier("MainList")! as GroceryListViewController // TODO: don't hardcode what list to go to.
         self.navigationController?.pushViewController(groceryList, animated: true)
     }
+    
+    // MARK: Homerolled Functions
     
     
     
